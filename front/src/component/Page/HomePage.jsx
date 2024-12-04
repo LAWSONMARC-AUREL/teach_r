f024018d2c4b4cdf09b04fbd647590d8553bfbbe
Marc-aurel Lawson <marc-aurel.lawson@etudiant.univ-reims.fr>
2024-12-04 23:17:22 +0100

    feat: homePage

--- front/src/component/Page/HomePage.jsx
+++ front/src/component/Page/HomePage.jsx
@@ -4,7 +4,8 @@ import TableProduct from "../Molecule/TableProduct.jsx";
 export default function HomePage(){
     return(
         <>
-           <h1>Bienvenue sur la page d'acceuil</h1>
+            <title>Teach'R</title>
+            <h1>Bienvenue sur la page d'acceuil</h1>
             <TableProduct/>
         </>
     )
